e99075a37d566aaac1d62eec9dad9a32dc2de722
lily-software <contact@lily.software>
2023-05-13 13:22:06 +0000

    modified graphic borders

--- OpenVapour/Graphics/Shadow.cs
+++ OpenVapour/Graphics/Shadow.cs
@@ -8,9 +8,10 @@ using Graphics = System.Drawing.Graphics;
 
 namespace OpenVapour.Graphics {
     internal class Shadow {
+        internal const int BorderRadius = 20;
         internal static Image AddOuterShadow(Image image, Color color) {
             Bitmap epicshadow = new Bitmap(image.Width, image.Height);
             using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(epicshadow)) { 
                 g.FillRectangle(new SolidBrush(color), new Rectangle(0, 0, image.Width, image.Height)); 
-                g.DrawImage(image, new Rectangle(image.Width / 20, image.Width / 20, image.Width - image.Width / 10, image.Height - image.Width / 10)); }
+                g.DrawImage(image, new Rectangle(BorderRadius, BorderRadius, image.Width - BorderRadius * 2, image.Height - BorderRadius * 2)); }
             return epicshadow; }}}
