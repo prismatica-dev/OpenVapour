cc0a67edc376a3c4b87bd4f0851e78615a4f8c23
lily-software <contact@lily.software>
2023-06-15 03:22:45 +0000

    [1.3.4c] change version

--- OpenVapour/Main.Designer.cs
+++ OpenVapour/Main.Designer.cs
@@ -195,9 +195,9 @@
             this.storeselect.ForeColor = System.Drawing.Color.White;
             this.storeselect.Location = new System.Drawing.Point(3, 0);
             this.storeselect.Name = "storeselect";
-            this.storeselect.Size = new System.Drawing.Size(373, 25);
+            this.storeselect.Size = new System.Drawing.Size(371, 25);
             this.storeselect.TabIndex = 0;
-            this.storeselect.Text = "OpenVapour v1.3.4b — FLOSS Torrent Search";
+            this.storeselect.Text = "OpenVapour v1.3.4c — FLOSS Torrent Search";
             this.storeselect.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.storeselect.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Drag);
             // 
